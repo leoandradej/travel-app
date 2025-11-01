b2fed2e44aaf6da36de1c01278abe3a3f42ac840
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-10-31 23:54:10 -0300

    Set up Sentry to keep track of errors

---
+++ instrument.server.mjs
@@ -0,0 +1,24 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+Sentry.init({
+  dsn: "https://4827d76861f527fbbda50e6dd03040ff@o4510117250859008.ingest.us.sentry.io/4510275923345408",
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+  // Add our Profiling integration
+  integrations: [nodeProfilingIntegration()],
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for tracing.
+  // We recommend adjusting this value in production
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#tracesSampleRate
+  tracesSampleRate: 1.0,
+  // Set profilesSampleRate to 1.0 to profile 100%
+  // of sampled transactions.
+  // This is relative to tracesSampleRate
+  // Learn more at
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#profilesSampleRate
+  profilesSampleRate: 1.0,
+});
