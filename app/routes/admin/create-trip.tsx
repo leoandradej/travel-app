a3521f060d56324c494242af46e5425b5ee26ae7
Leonardo Andrade <105254884+leoandradej@users.noreply.github.com>
2025-11-03 21:09:25 -0300

    Implemented display all trips functionality

--- app/routes/admin/create-trip.tsx
+++ app/routes/admin/create-trip.tsx
@@ -94,7 +94,7 @@ const CreateTrip = ({ loaderData }: Route.ComponentProps) => {
         body: JSON.stringify({
           country: formData.country,
           numberOfDays: formData.duration,
-          travelStyles: formData.travelStyle,
+          travelStyle: formData.travelStyle,
           interests: formData.interest,
           budget: formData.budget,
           groupType: formData.groupType,
